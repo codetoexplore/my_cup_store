c48073c9e805c94a1a4152b323c1f0b467fa38cf
Giedrius Bernotavicius <xgiedrius@yahoo.com>
2019-05-21 21:59:34 +0100

    best_sellers_update + giving handle as trigger

--- src/components/best_sellers.vue
+++ src/components/best_sellers.vue
@@ -22,14 +22,20 @@
                 v-else
                 :key="item.title"
                 avatar
-                @click.stop="displaydialog(item.title)"
+                @click.stop="displaydialog(item.handle)"
               >
                 <v-list-tile-avatar>
                   <img :src="item.avatar">
                 </v-list-tile-avatar>
 
                 <v-list-tile-content>
-                  <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                
+            
+                      <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                      <v-list-tile-handle v-html="item.handle"></v-list-tile-handle>
+             
+  
+                  
                   <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
                 </v-list-tile-content>
               </v-list-tile>
@@ -51,36 +57,38 @@ import { eventBus } from '../main';
         items: [
           { header: 'All Time' },
           {
-            avatar: 'https://pbs.twimg.com/profile_images/952394640552005632/zsjSs7yd_bigger.jpg',
-            title: 'm.diane',
-            subtitle: `<span class='text--primary'>@cULTMOTHER</span> &mdash; told my boyfriend I was going to start my period and he said, "AGAIN??" it's like, you know what, you're right, I'm cancelling my subscription.`
+            avatar: 'https://pbs.twimg.com/profile_images/1096031894041284609/Wb77WUdv_400x400.jpg',
+            title: 'Katy Perry',
+            handle: '@kattyperry',
+            subtitle: `I LOVE MY MOM`
           },
           { divider: true, inset: true },
           {
-            avatar: 'https://pbs.twimg.com/profile_images/1120298627186950145/gp3dGnMf_400x400.jpg',
-            title: 'ari',
-            subtitle: `<span class='text--primary'>@aarifah_l</span> &mdash; Your pornstar name is:
-            Your first name + your last name
-            Bc it's you. You're a hoe.`
+            avatar: 'https://pbs.twimg.com/profile_images/822547732376207360/5g0FC8XX_400x400.jpg',
+            title: 'Barack Obama',
+            handle: '@BarackObama',
+            subtitle: `I’ve always drawn inspiration from what Dr. King called life’s most persistent and urgent question: "What are you doing for others?"...`
           },
           { divider: true, inset: true },
           {
-            avatar: 'https://pbs.twimg.com/profile_images/1122672313646796800/ld3pL6kp_reasonably_small.jpg',
-            title: 'khiry curving',
-            subtitle: `<span class='text--primary'>@KhiryArion</span> &mdash; so I asked my mom why is she still talking to my old boo and she asked me “why are you still talking to my ex husband?”. Ma’am.. that is my father 
-😂`
+            avatar: 'https://pbs.twimg.com/profile_images/1053330932906639362/9OfK5qOh_400x400.jpg',
+            title: 'Ellen DeGeneres',
+            handle: '@TheEllenShow',
+            subtitle: `The #royalbaby is here! Congratulations, Meghan Markle and Prince Harry. The baby is 7th in line for the throne, which is crazy...`
           },
           { divider: true, inset: true },
           {
-            avatar: 'https://pbs.twimg.com/profile_images/1103514236494344192/aTx9f9fJ_reasonably_small.jpg',
-            title: 'Alec',
-            subtitle: `<span class='text--primary'>@aIecrl</span> &mdash; Today I had 800 mg of caffeine, exercised for 2 hours, ate literally 80 pizza rolls, and did a facemask. The line between self care and self destruction is a fine one but god do I walk it hard brother`
+            avatar: 'https://pbs.twimg.com/profile_images/1127326694585421826/kuFXswNr_400x400.jpg',
+            title: 'Ariana Grande',
+            handle: '@ArianaGrande',
+            subtitle: `love u`
           },
           { divider: true, inset: true },
           {
-            avatar: 'https://pbs.twimg.com/profile_images/1110941074116141057/XyLc6OW7_bigger.jpg',
-            title: 'evelyn',
-            subtitle: `<span class='text--primary'>@evelynnleee</span> &mdash; this customer walked in 10 SECONDS before we CLOSED and she had the audacity to yell “yay i made it” bitch you made what? made me mad?`
+            avatar: 'https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_400x400.jpg',
+            title: 'Donald J. Trump',
+            handle: `@realDonaldTrump`,
+            subtitle: `MAKE AMERICA GREAT AGAIN!` 
           }
         ]
       }
@@ -91,8 +99,8 @@ import { eventBus } from '../main';
         console.log(parent_el)
         
       },
-      displaydialog(itemtitle) {
-        eventBus.$emit('componentactivated', itemtitle);
+      displaydialog(itemhandle) {
+        eventBus.$emit('componentactivated', itemhandle);
       }
     },
     components: {
--- src/components/dialog.vue
+++ src/components/dialog.vue
@@ -4,16 +4,10 @@
                 max-width="290"
             >
               <v-card>
-                <v-img> </v-img>
+                <v-img :src="require('../assets/cups/mockup_donald.jpg')">
+                </v-img>
 
-                <v-card-text>
-                   This sturdy mug is perfect for your morning coffee, afternoon tea, or whatever hot beverage you enjoy. It's glossy white and yields vivid prints that retain their quality when dish-washed and microwaved.
-                      <ul>
-                      <li>Ceramic</li>
-                      <li>Height - 3.85" (9.8 cm), diameter - 3.35" (8.5 cm)</li>
-                      <li>Dishwasher and microwave safe</li>
-                      </ul>
-                </v-card-text>
+                
 
               <v-card-actions>
                   <v-spacer></v-spacer>
